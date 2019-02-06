6283d265b62bb769918a04a09a8ebc985c97fc27
limer01 <lalor95@gmail.com>
2019-02-06 23:27:50 +0000

    Added Quit and Play Button funtionality

---
+++ src/game/MouseInput.java
@@ -0,0 +1,50 @@
+package game;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseInput implements MouseListener{
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+//        public Rectangle playButton = new Rectangle(Game.WIDTH/2 + 120, 150, 100, 50);
+//    public Rectangle helpButton = new Rectangle(Game.WIDTH/2 + 120, 250, 100, 50);
+//    public Rectangle quitButton = new Rectangle(Game.WIDTH/2 + 120, 350, 100, 50);
+        int mouseX = e.getX();
+        int mouseY = e.getY();
+        
+        //Play Button
+        if(mouseX >= Game.WIDTH/2 + 120 && mouseX <=Game.WIDTH/2 + 220){
+            if(mouseY >= 150 && mouseY <= 300){
+                Game.State = Game.State.GAME;
+            }
+        }
+        
+        if(mouseX >= Game.WIDTH/2 + 120 && mouseX <=Game.WIDTH/2 + 220){
+            if(mouseY >= 350 && mouseY <= 400){
+                System.exit(1);
+            }
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
